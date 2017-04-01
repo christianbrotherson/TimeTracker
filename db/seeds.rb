aa7e148492d93d4c58f8f4749ce9ba6561e89cbd
Christian Brotherson <christian.brotherson@gmail.com>
2017-03-31 21:47:54 -0600

    Implemented labels with status values on index for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,20 @@ AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirm
 
 puts "1 AdminUser created"
 
-100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+34.times do |post|
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, status: "submitted")
 end
 
-puts "100 Posts have been created"
+puts "34 submitted Posts have been created"
+
+33.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, status: "approved")
+end
+
+puts "33 approved Posts have been created"
+
+33.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, status: "rejected")
+end
+
+puts "33 rejected Posts have been created"
