08a25c7e98dd2b4fe2cadeb0d8e1892356844ca6
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-04 15:08:14 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ end
 puts "33 rejected Posts have been created"
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
-puts "100 AuditLogs have been created"
+puts "100 audit logs have been created"
