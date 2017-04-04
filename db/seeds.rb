e9d3c4559c8216708ddeaf26c3db681e522bbde6
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-04 14:39:34 -0600

    Updated the audit_log factory and tested out 6 day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,9 @@ puts "33 approved Posts have been created"
 end
 
 puts "33 rejected Posts have been created"
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 AuditLogs have been created"
