39344146e59ed7d092195eb42b069c639fd1c586
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-12 11:07:29 -0600

    Integrated callback for rejections

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,21 @@ class Post < ApplicationRecord
 	belongs_to :user
 	validates_presence_of :date, :rationale, :overtime_request
   validates :overtime_request, numericality: { greater_than: 0.0 }
-
   scope :posts_by, ->(user) { where(user_id: user.id) }
-
   after_save :update_audit_log
+  after_save :confirm_audit_log, if: :submitted?
+  after_save :un_confirm_audit_log, if: :rejected?
 
   private
+    def confirm_audit_log
+      audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+      audit_log.confirmed! if audit_log
+    end
+
+    def un_confirm_audit_log
+      audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+      audit_log.pending! if audit_log
+    end
 
     def update_audit_log
       audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
